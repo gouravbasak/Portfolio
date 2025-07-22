86c1b6de1a901ca5aabbc3129208a1abc27f449e
gourav basak <gouravbasak@gouravs-MacBook-Air.local>
2025-07-22 20:34:51 +0530

    figma changes

--- src/Components/AboutMe/Tools.jsx
+++ src/Components/AboutMe/Tools.jsx
@@ -6,7 +6,7 @@ import github from '../../assets/git.png';
 import azure from '../../assets/ado.png';
 import netlify from '../../assets/netlify.png';
 import cicd from '../../assets/cicd.png';
-import figma from '../../assets/FTigma.png';
+import figma from '../../assets/Figma.png';
 import jest from '../../assets/jest.png';
 const Tools = () => {
   return (
